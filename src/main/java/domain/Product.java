dc5e4a399f2c92e44dbb6d8e99143ca947aa052e
yannikkel <78969123+yannikkel@users.noreply.github.com>
2021-06-03 19:01:52 +0300

    domain

---
+++ src/main/java/domain/Product.java
@@ -0,0 +1,15 @@
+package domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class Product {
+    private int id;
+    private String name;
+    private int price;
+}
