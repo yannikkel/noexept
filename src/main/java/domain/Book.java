4f598fffc068bb6ff7b6394d3a5dd6b5a9c84216
yannikkel <78969123+yannikkel@users.noreply.github.com>
2021-06-03 19:01:21 +0300

    domain

---
+++ src/main/java/domain/Book.java
@@ -0,0 +1,20 @@
+package domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+
+public class Book extends Product {
+    private String author;
+
+    public Book(String author) {
+        this.author = author;
+    }
+
+    public Book(int id, String name, int price, String author) {
+        super(id, name, price);
+        this.author = author;
+    }
+}
